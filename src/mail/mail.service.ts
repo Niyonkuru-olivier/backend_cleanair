84a3167f1481ed54954161b2c5be36878aa841eb
Niyonkuru-olivier <oniyonkuru44@gmail.com>
2026-05-18 13:25:34 +0200

    fix: force nodemailer IPv4 family and port 465

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -14,12 +14,14 @@ export class MailService {
   constructor(private configService: ConfigService) {
     this.transporter = nodemailer.createTransport({
       host: this.configService.get<string>('SMTP_HOST'),
-      port: this.configService.get<number>('SMTP_PORT'),
-      secure: false, // true for 465, false for other ports
+      port: 465, // Force 465 (Implicit SSL) which is more reliable on Render
+      secure: true, // true for 465
       auth: {
         user: this.configService.get<string>('SMTP_USER'),
         pass: this.configService.get<string>('SMTP_PASS'),
       },
+      // This forces Nodemailer to bypass IPv6 completely during connection
+      ...({ family: 4 } as any),
     });
   }
 
